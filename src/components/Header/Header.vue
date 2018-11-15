32888909703140ba8b8a9d801c311e8dea50a0e6
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-15 23:11:21 +0100

    Add header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="black">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">Offchan</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item
+          v-for="(category, index) in categories"
+          tag="router-link"
+          :key="index"
+          :to="`/board/${category.board}/`"
+        >
+          {{ category.board }}
+        </b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import * as config from "@/config";
+
+@Component({})
+export default class Header extends Vue {
+  categories = config.categories;
+}
+</script>
