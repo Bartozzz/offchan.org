89607d0c40c02dad9571f7e0cd575e867bae5e21
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-15 23:26:32 +0100

    Add error page mockup

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="view">
-    Error page
+  <div class="error">
+    <p class="error-message">An error occurred, please refresh</p>
   </div>
 </template>
 
@@ -10,3 +10,36 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({})
 export default class ErrorView extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.error {
+  position: relative;
+
+  background-image: url(../assets/error-1.gif);
+  background-size: contain;
+  background-position: bottom;
+
+  image-rendering: pixelated;
+  image-rendering: -moz-crisp-edges;
+  image-rendering: -o-crisp-edges;
+  image-rendering: -webkit-optimize-contrast;
+  -ms-interpolation-mode: nearest-neighbor;
+
+  width: 100vw;
+  height: 100vh;
+
+  &-message {
+    position: absolute;
+    top: 3rem;
+    left: 3rem;
+
+    padding: 1rem 2rem;
+    margin: 0;
+
+    font-size: 4rem;
+
+    background-color: #cb5f75;
+    color: #ffffff;
+  }
+}
+</style>
