b281edfbb25935b072ba4878cac7b5abe3feb94d
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-15 22:31:56 +0100

    Add basic home mockup

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,27 @@
 </template>
 
 <style lang="scss">
+*,
+*:before,
+*:after {
+  /* Make transparent link selection, adjust last value opacity 0 to 1.0 */
+  -webkit-tap-highlight-color: rgba(black, 0);
+}
+
 #app {
+  position: absolute;
+
+  height: 100%;
+  width: 100%;
+
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  /* Prevent callout to copy image, etc when tap to hold */
+  -webkit-touch-callout: none;
+
+  /* Prevent webkit from resizing text to fit */
+  -webkit-text-size-adjust: none;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
-  <div class="view">
-    Home page
-  </div>
+  <b-container fluid class="h-100">
+    <b-row class="h-100" align-v="center">
+      <b-col>
+        <ul>
+          <li v-for="(category, index) in categories" :key="index">
+            <router-link :to="`/board/${category.board}/`" :title="category.description">
+              /{{ category.board }}/ – {{ category.name }}
+            </router-link>
+          </li>
+        </ul>
+      </b-col>
+
+      <b-col>
+        <img alt="She's watching" src="../assets/lain/1.gif" />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  categories = [
+    { board: "cyb", name: "Cyberia", description: "Random" },
+    { board: "psy", name: "Psychology", description: "" },
+    { board: "art", name: "Art & Design", description: "" },
+    { board: "cult", name: "Culture & Media", description: "" },
+    { board: "tech", name: "Technology & Science", description: "" },
+    { board: "λ", name: "Programming", description: "" },
+    { board: "Δ", name: "Projects", description: "" }
+  ];
+}
 </script>
+
+<style lang="scss" scoped>
+</style>
