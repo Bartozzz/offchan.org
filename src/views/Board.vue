fe93b9fc59a241d8e20094d1e85036abfaae6eef
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-15 23:12:01 +0100

    Add Board page mockup

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,12 +1,61 @@
 <template>
   <div class="view">
-    Board page
+    <Header />
+
+    <b-container>
+      <b-jumbotron bg-variant="black" text-variant="white" class="jumbotron">
+        <template slot="header">
+          /{{ category.board }}/ – {{ category.name }}
+        </template>
+
+        <template slot="lead">
+          {{ category.description }}
+        </template>
+
+        <hr class="my-4">
+
+        Tutaj form dodawania threadu
+      </b-jumbotron>
+    </b-container>
+
+    <b-container>
+      Board page
+    </b-container>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import * as config from "@/config";
+import Header from "@/components/Header/Header.vue";
 
-@Component({})
-export default class BoardView extends Vue {}
+@Component({
+  components: {
+    Header
+  }
+})
+export default class BoardView extends Vue {
+  get category() {
+    return config.categories.find(
+      category => category.board === this.$route.params.category
+    );
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.jumbotron {
+  // Head & lead
+  & > h1,
+  & > p {
+    text-align: center;
+
+    margin: 1rem auto;
+    padding: 0;
+  }
+
+  & > p {
+    max-width: 600px;
+  }
+}
+</style>
