82f9963ee2294c1f1c8914068f5e707fc45dcaed
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-16 20:15:46 +0100

    Create comment components

--- src/components/Thread/Post.vue
+++ src/components/Thread/Post.vue
@@ -1,39 +1,48 @@
 <template>
-  <b-card tag="article" class="thread" no-body>
-    <b-row no-gutters>
-      <b-col lg="2" md="3" sm="12">
-        <slot name="upload-file"></slot>
-      </b-col>
+  <div>
+    <b-card tag="article" class="thread" no-body>
+      <b-row no-gutters>
+        <b-col lg="2" md="3" sm="12">
+          <slot name="upload-file"></slot>
+        </b-col>
 
-      <b-col>
-        <header class="thread-header text-muted">
-          <slot name="author">Anon</slot>
-        </header>
+        <b-col>
+          <header class="thread-header text-muted">
+            <span class="thread-id">#{{ guid }}</span>
+            <slot name="author">Anon</slot>
+          </header>
 
-        <b-card-body class="thread-content">
-          <slot></slot>
-        </b-card-body>
-      </b-col>
-    </b-row>
+          <b-card-body class="thread-content">
+            <slot></slot>
+          </b-card-body>
+        </b-col>
+      </b-row>
 
-    <b-card-footer class="w-100 text-muted">
-      <section class="float-left">
-        <slot name="upload-name"></slot>
-      </section>
+      <b-card-footer>
+        <section class="text-right">
+          <a href="#" class="card-link">View</a>
+          <a href="#" class="card-link">Reply</a>
+        </section>
+      </b-card-footer>
+    </b-card>
 
-      <section class="text-right">
-        <a href="#" class="card-link">View</a>
-        <a href="#" class="card-link">Reply</a>
-      </section>
-    </b-card-footer>
-  </b-card>
+    <CommentsList :data="comments" />
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
+import CommentsList from "@/components/Comments/List.vue";
 
-@Component({})
-export default class ThreadPost extends Vue {}
+@Component({
+  components: {
+    CommentsList
+  }
+})
+export default class ThreadPost extends Vue {
+  @Prop(String) guid!: string;
+  @Prop(Array) comments!: Array<Object>;
+}
 </script>
 
 <style lang="scss" scoped>
