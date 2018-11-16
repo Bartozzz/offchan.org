bd62e0050943a7b9c3c12bf636c0bbce4d91136d
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-16 22:38:30 +0100

    Change home page styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <b-container fluid class="h-100">
     <b-row class="h-100" align-v="center">
-      <b-col>
-        <ul>
-          <li v-for="(category, index) in categories" :key="index">
-            <router-link :to="`/board/${category.board}/`" :title="category.description">
-              /{{ category.board }}/ – {{ category.name }}
-            </router-link>
-          </li>
-        </ul>
+      <b-col class="d-none d-md-block">
+        <img class="lain" alt="She's watching" src="../assets/lain/1.gif" />
       </b-col>
 
       <b-col>
-        <img alt="She's watching" src="../assets/lain/1.gif" />
+        <ul class="boards">
+          <li v-for="(category, index) in categories" :key="index">
+            <router-link
+              :to="`/board/${category.board}/`"
+              :title="category.description"
+              :data-text="`/${category.board}/ – ${category.name}`"
+              class="board"
+            >
+              <span class="board-name">/{{ category.board }}/ – {{ category.name }}</span>
+              <span class="board-desc">{{ category.description }}</span>
+            </router-link>
+          </li>
+        </ul>
       </b-col>
     </b-row>
   </b-container>
@@ -29,4 +35,72 @@ export default class HomeView extends Vue {
 </script>
 
 <style lang="scss" scoped>
+@import "../styles/mixins/glitch";
+
+$glitch-intensity: 5;
+$glitch-text-color: #c1b290;
+$glitch-background: #111111;
+$glitch-color-a: black;
+$glitch-color-b: black;
+$glitch-width: 200;
+$glitch-height: 50;
+
+.lain {
+  width: 100%;
+}
+
+.boards {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+
+  align-items: flex-start;
+  justify-content: left;
+
+  height: 100%;
+  width: 100%;
+
+  list-style: none;
+
+  &,
+  & > li {
+    margin: 0;
+    padding: 0;
+  }
+}
+
+.board {
+  display: block;
+
+  font-size: 4.5vmin;
+  font-weight: bold;
+  text-decoration: none;
+
+  opacity: 0.75;
+  color: #844657;
+
+  &:hover {
+    @include textGlitch(
+      "logo",
+      $glitch-intensity,
+      $glitch-text-color,
+      $glitch-background,
+      $glitch-color-a,
+      $glitch-color-b,
+      $glitch-width,
+      $glitch-height
+    );
+  }
+
+  &-name,
+  &-desc {
+    display: block;
+  }
+
+  &-desc {
+    font-size: 2vmin;
+
+    opacity: 0.5;
+  }
+}
 </style>
