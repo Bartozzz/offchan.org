c4ee88a281ad81c0e77cf5c48df7a69d4294fda1
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-17 22:51:47 +0100

    Add and force anonymous auth before accessing

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,38 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { auth } from "./api/firebase";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+function handleAuthFailure() {
+  (<Element>document.querySelector("#app")).innerHTML = `
+    You cannot enter Offchan.
+  `;
+}
+
+function handleAuthSuccess() {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount("#app");
+}
+
+/**
+ * Render Vue application only if auth succeded. This is because Firebase rules
+ * require the user to be logged-in.
+ */
+auth.onAuthStateChanged(user => {
+  if (user) {
+    handleAuthSuccess();
+  } else {
+    handleAuthFailure();
+  }
+});
+
+/**
+ * Force log-in anonymouly.
+ */
+auth.signInAnonymously().catch(handleAuthFailure);
