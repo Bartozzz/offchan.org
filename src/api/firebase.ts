1cd2547de06a1545a585977a547ffc9f5488499f
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-17 22:09:53 +0100

    Initialize firebase app with .env variables

---
+++ src/api/firebase.ts
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+export const app = firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+});
