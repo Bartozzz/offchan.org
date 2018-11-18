a67b30931d2c779abff81e6d1c09a1e806b53d82
Florian3k <24961583+Florian3k@users.noreply.github.com>
2018-11-18 03:36:49 +0100

    create initialState for store

---
+++ src/store/initialState.ts
@@ -0,0 +1,45 @@
+export default {
+  boards: {
+    cyb: {
+      name: "Cyberia",
+      description: "Random. Low life. High tech. Anonymity. Privacy. Security.",
+      threads: []
+    },
+    psy: {
+      name: "Psychology",
+      description: "Dreams. Drgs. Consciousness. Feelings and experiences.",
+      threads: []
+    },
+    art: {
+      name: "Art & Design",
+      description: "Art. Design. Origami. Photography. Writing.",
+      threads: []
+    },
+    cult: {
+      name: "Culture & Media",
+      description:
+        "Anime. Fashion. Films. Games. Lifestyle. Lliterature. Music.",
+      threads: []
+    },
+    tech: {
+      name: "Technology & Science",
+      description: "Science. Technology. Engineering. Mathematics.",
+      threads: []
+    },
+    λ: {
+      name: "Programming",
+      description: "Structure and interpretation of computer programs.",
+      threads: []
+    },
+    Δ: {
+      name: "Projects",
+      description: "Do-it-yourself. Hardware, Software and Community Projects.",
+      threads: []
+    },
+    φ: {
+      name: "Philosophy",
+      description: "Philosophy. Share your thoughts.",
+      threads: []
+    }
+  }
+};
