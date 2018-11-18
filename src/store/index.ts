057f5a9f433c951cdbd0828255844dc56f74ed3a
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-18 19:37:39 +0100

    Remove useless modules

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { State } from './types';
-import { getInitialState } from './getInitialState';
+import { State } from "./types";
+import { getInitialState } from "./getInitialState";
 
 Vue.use(Vuex);
 
