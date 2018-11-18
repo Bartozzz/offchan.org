d449c5bd8950784b8400fa4ef880a11aa1cd9e0d
Florian3k <24961583+Florian3k@users.noreply.github.com>
2018-11-18 03:37:21 +0100

    use types and initialState in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import initialState from "./initialState";
+import { State } from "./types";
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
+export default new Vuex.Store<State>({
+  state: initialState,
+  getters: {},
   mutations: {},
   actions: {}
 });
