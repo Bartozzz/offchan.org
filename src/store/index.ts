aefd879bec25352f296fed7ec344c028a4fae1f7
Florian3k <24961583+Florian3k@users.noreply.github.com>
2018-11-18 13:03:51 +0100

    add Category type to categories getter

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,14 @@ import Vuex from "vuex";
 
 import initialState from "./initialState";
 import { State } from "./types";
+import { Category } from '@/config';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store<State>({
   state: initialState,
   getters: {
-    categories(state) {
+    categories(state): Category[] {
       return Object.entries(state.boards).map(
         ([board, { name, description }]) => ({ board, name, description })
       )
