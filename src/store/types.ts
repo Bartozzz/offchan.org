9633ef09e16cb524214398dc61c3e84ac1a21f65
Florian3k <24961583+Florian3k@users.noreply.github.com>
2018-11-18 03:36:34 +0100

    add basic types for store

---
+++ src/store/types.ts
@@ -0,0 +1,26 @@
+export type BoardNames = "cyb" | "psy" | "art" | "cult" | "tech" | "λ" | "Δ" | "φ";
+
+export interface State {
+  boards: {
+    [BoardName in BoardNames]: Board 
+  };
+}
+
+export interface Board {
+  name: string;
+  description: string;
+  threads: Thread[];
+}
+
+export interface Thread {
+  guid: string;
+  author: string;
+  content: string;
+  comments: Comment[];
+}
+
+export interface Comment {
+  guid: string;
+  author: string;
+  content: string;
+}
