84d1df5a06a0af545724a3b3eb21caf84415d6dc
Florian3k <24961583+Florian3k@users.noreply.github.com>
2018-11-23 18:46:44 +0100

    create createThread action

--- src/store/threads/actions.ts
+++ src/store/threads/actions.ts
@@ -1,6 +1,15 @@
-import { threadsCollection } from '@/api/firebase';
-import { ActionTree } from 'vuex';
-import { State, BoardNames } from '@/store/types';
+import { threadsCollection } from "@/api/firebase";
+import { firestore } from "firebase";
+import { ActionTree } from "vuex";
+import { State, BoardNames } from "@/store/types";
+
+interface createThreadDto {
+  content: string;
+  author?: string;
+  file?: string;
+  board: string;
+  createdAt: firestore.FieldValue;
+}
 
 export const threadActions: ActionTree<State, {}> = {
   fetchThreads({ commit, state }, { board }: { board: BoardNames }) {
@@ -23,5 +32,25 @@ export const threadActions: ActionTree<State, {}> = {
           console.error("fetchThreads error: ", error);
         }
       );
+  },
+  createThread({}, { name, content, file, board }) {
+    // TODO - handle file upload
+    const newThread: createThreadDto = {
+      content,
+      board,
+      createdAt: firestore.FieldValue.serverTimestamp()
+    };
+    if (name) {
+      newThread.author = name;
+    }
+    threadsCollection
+      .add(newThread)
+      .then(doc => {
+        // TODO - handle success
+      })
+      .catch(err => {
+        console.error(err);
+        // TODO - handle error
+      });
   }
 };
