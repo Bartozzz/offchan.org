022ae6b95377dae340b473a88d30555095cad703
Florian3k <24961583+Florian3k@users.noreply.github.com>
2018-11-23 18:19:59 +0100

    create setThread mutation

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,12 @@ import Vue from "vue";
 import Vuex from "vuex";
 import { State } from "./types";
 import { getInitialState } from "./getInitialState";
+import { threadMutations } from './threads/mutations';
 
 Vue.use(Vuex);
 
+
 export default new Vuex.Store<State>({
   state: getInitialState(),
-  mutations: {},
-  actions: {}
+  mutations: { ...threadMutations }
 });
