e75cc480b9f99ab4145eab80c19023c92d982506
Bartosz Łaniewski <bartosz@laniewski.me>
2018-11-24 12:23:25 +0100

    Add validation in comments form

--- src/components/Comments/Form.vue
+++ src/components/Comments/Form.vue
@@ -3,13 +3,34 @@
     <b-row class="h-100" align-v="center">
       <b-col lg="6" md="7" sm="12">
         <b-form-group label="Optional name:" label-for="name">
-          <b-form-input v-model="form.name" id="name" type="text" placeholder="Enter your name (optional)…" />
+          <b-form-input
+            v-model="form.name"
+            :state="getFieldState('name')"
+            id="name"
+            type="text"
+            placeholder="Enter your name (optional)…"
+            aria-describedby="name-feedback"
+          />
+
+          <b-form-invalid-feedback id="name-feedback">
+            {{ errors.name }}
+          </b-form-invalid-feedback>
         </b-form-group>
       </b-col>
 
       <b-col>
         <b-form-group label="Optional file:" label-for="file">
-          <b-form-file v-model="form.file" id="file" placeholder="Choose a file (optional)..." />
+          <b-form-file
+            v-model="form.file"
+            :state="getFieldState('file')"
+            id="file"
+            placeholder="Choose a file (optional)..."
+            aria-describedby="file-feedback"
+          />
+
+          <b-form-invalid-feedback id="file-feedback">
+            {{ errors.file }}
+          </b-form-invalid-feedback>
         </b-form-group>
       </b-col>
     </b-row>
@@ -18,7 +39,20 @@
       label="Message:"
       label-for="content"
     >
-      <b-form-textarea v-model="form.content" id="content" rows="3" max-rows="6" placeholder="Enter your message…" />
+      <b-form-textarea
+        v-model="form.content"
+        :state="getFieldState('content')"
+        @keyup.enter.native="onSubmit"
+        id="content"
+        rows="3"
+        max-rows="6"
+        placeholder="Enter your message…"
+        aria-describedby="content-feedback"
+      />
+
+      <b-form-invalid-feedback id="content-feedback">
+        {{ errors.content }}
+      </b-form-invalid-feedback>
     </b-form-group>
 
     <b-button class="float-right" type="submit" variant="primary">Post</b-button>
@@ -28,9 +62,16 @@
 <script lang="ts">
 import { Component, Vue, Prop, Watch } from "vue-property-decorator";
 import { nameStore, formStore } from "@/store/local";
+import { normalizeString } from "@/helpers/validators";
 
-type MaybeString = string | null;
-type MaybeFile = File | null;
+type MaybeString = string | null | void;
+type MaybeFile = File | null | void;
+
+interface FormErrors {
+  file: MaybeString;
+  name: MaybeString;
+  content: MaybeString;
+}
 
 interface FormData {
   file: MaybeFile;
@@ -43,15 +84,51 @@ export default class CommentForm extends Vue {
   @Prop(String) guid!: string;
   @Prop(String) board!: string;
 
-  get uniqueCommentID() {
-    return `${this.board}-${this.guid}`;
-  }
+  errors: FormErrors = {
+    name: null,
+    file: null,
+    content: null
+  };
 
   form: FormData = {
     name: null,
     file: null,
     content: null
   };
+
+  get uniqueCommentID() {
+    return `${this.board}-${this.guid}`;
+  }
+
+  // Returns undefined if there's no error for a field:
+  getFieldState(field: keyof FormErrors) {
+    if (this.errors[field]) {
+      return false;
+    }
+  }
+
+  resetErrors() {
+    this.errors.content = null;
+    this.errors.file = null;
+    this.errors.name = null;
+  }
+
+  resetForm() {
+    this.form.content = null;
+    this.form.file = null;
+
+    // NOTE: don't clear name since it should be persistent for the session:
+    // this.form.name = null;
+  }
+
+  validateForm(data: FormData) {
+    if (!data.content) {
+      this.errors.content = "Message is required.";
+      return false;
+    }
+
+    return true;
+  }
 
   storeData(data: FormData) {
     formStore.setItem<MaybeString>(this.uniqueCommentID, data.content);
@@ -61,26 +138,39 @@ export default class CommentForm extends Vue {
     }
   }
 
-  sendData({ name, file, content }: FormData) {
-    if (!content) {
-      return;
+  sendData(data: FormData) {
+    if (this.validateForm(data)) {
+      this.resetErrors();
+      this.resetForm();
+
+      // Reset local storage:
+      formStore.setItem<MaybeString>(this.uniqueCommentID, null);
+
+      // Send thread:
+      this.$store.dispatch("createComment", {
+        threadId: this.guid,
+        name: data.name,
+        file: data.file,
+        content: data.content
+      });
     }
-    this.$store.dispatch("createComment", {
-      threadId: this.guid,
-      name,
-      file,
-      content
-    });
   }
 
   onSubmit(event: Event) {
     event.preventDefault();
     event.stopPropagation();
 
+    // Normalize form data for further validation:
+    const form: FormData = {
+      content: normalizeString(this.form.content),
+      name: normalizeString(this.form.name),
+      file: this.form.file
+    };
+
     if (navigator.onLine) {
-      this.sendData({ ...this.form });
+      this.sendData(form);
     } else {
-      this.storeData({ ...this.form });
+      this.storeData(form);
     }
   }
 
