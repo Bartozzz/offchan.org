61aa381f9caa90bcb9767aeac966e653b1c3843c
Bartosz Łaniewski <bartosz@laniewski.me>
2018-12-10 00:31:51 +0100

    Write fetch action in firebase counter document

--- src/api/firebase/document/counter.ts
+++ src/api/firebase/document/counter.ts
@@ -1,15 +1,63 @@
-export function create() {
+import { database } from "../app";
+import { Board } from "../../types";
+
+// Common counter document types:
+export type ThreadsCounter = { [BoardName in Board]: number };
+
+// Common counter document names:
+export const COUNTER_THREADS = "threads";
+
+export const counters = database.collection("counters");
+
+/**
+ * Does nothing right now. Stays here for consistency. Might be used in the
+ * future.
+ *
+ * @param document    Counter document identifier
+ * @param data        Counter payload
+ * @deprecated
+ */
+export function create<T = any>(document: string, data: T) {
   throw new Error("Not implemented");
 }
 
-export function fetch() {
+/**
+ * Fetches all data from a given counter.
+ *
+ * @param document    Counter document identifier
+ * @param onSnapshot  Callback
+ */
+export function fetch<T = any>(
+  document: string,
+  onSnapshot: (data: Readonly<T>) => void
+) {
+  const counterQuery = counters.doc(document);
+  const unsubscribe = counterQuery.onSnapshot(snapshot => {
+    onSnapshot(snapshot.data() as Readonly<T>);
+  });
+
+  return unsubscribe;
+}
+
+/**
+ * Does nothing right now. Stays here for consistency. Might be used in the
+ * future.
+ *
+ * @param document    Counter document identifier
+ * @param data        Counter payload
+ * @deprecated
+ */
+export function update<T = any>(document: string, data: T) {
   throw new Error("Not implemented");
 }
 
-export function update() {
-  throw new Error("Not implemented");
-}
-
-export function remove() {
+/**
+ * Does nothing right now. Stays here for consistency. Might be used in the
+ * future.
+ *
+ * @param document    Counter document identifier
+ * @deprecated
+ */
+export function remove(document: string) {
   throw new Error("Not implemented");
 }
